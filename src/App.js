a1e6b9d85a01efb090870ecb32d1c3456840936a
FernandoVelcic <FernandoVelcic@users.noreply.github.com>
2021-12-31 05:03:33 -0300

    Improve console height in mobile

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Console from './Console'
 const App = () => (
   <div class="flex flex-col h-screen">
     <Header></Header>
-    <div class="bg-manjaro bg-center p-5 h-full sm:p-20">
+    <div class="bg-manjaro bg-center pt-5 px-5 pb-16 sm:p-20 h-full">
       <Console></Console>
     </div>
   </div>

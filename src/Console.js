99589482b727311b462760560055d7f5587e405b
FernandoVelcic <fvelcic@gmail.com>
2021-12-31 03:30:05 -0300

    Improve console responsive

--- src/Console.js
+++ src/Console.js
@@ -1,5 +1,5 @@
 const Console = () => (
-  <div class="flex justify-end flex-col text-center h-[38rem] bg-black opacity-90">
+  <div class="flex justify-end flex-col text-center h-full bg-black opacity-90">
     <div class="p-1 font-mono text-left text-white">
       Hello my name is Fernando Velcic<br/>
       Welcome to my page<br/>

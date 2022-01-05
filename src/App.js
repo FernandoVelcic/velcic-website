9c655d814fed426627ef46557a3ed5f1fafa304e
FernandoVelcic <fvelcic@gmail.com>
2022-01-05 16:43:16 -0300

    Change my description, autotyper speed and add stack command

--- src/App.js
+++ src/App.js
@@ -9,6 +9,19 @@ const links = [
   { name: "Contact", url: "mailto:fvelcic@gmail.com" },
 ];
 
+const description = `
+  I'm Fernando Velcic, a passionate Software Engineer that looks for developing software quality product.
+
+  Professor at the university who aims to help people to understand how the computer works and help them to grow up in this field.
+
+  I consider myself a low level programmer who applies this knowledge to the new technologies, this helps me to have a different point of view when solving problems.
+  
+  I enjoy developing low level projects and high level as well, frontend and backend but also infrastructure, more than 15 years of programming experience in different technologies.
+  
+  If you want to know more about me please type <b>help</b>
+
+  ~ >>>\t`;
+
 const commandHandler = (command, stdout) => {
   const sanitized_command = command.toLowerCase().trim();
 
@@ -23,6 +36,27 @@ const commandHandler = (command, stdout) => {
     return stdout;
   } else if (sanitized_command === "clear") {
     return "";
+  } else if (sanitized_command === "help") {
+    const description = `
+    Useful commands:
+    - stack (to know more about my stack of technologies)
+    - linkedin (to see my work experience)
+    - github (to checkout some of my projects)
+    - youtube (to watch videos of me teaching python or computer organization)
+    - clear (to clear the terminal screen)
+    `;
+    return stdout + "\n" + description;
+  } else if (sanitized_command === "stack") {
+    const description = `
+    I like to be up to date, so I have tested different technologies over the years but the most relevant are the following:
+    - reactjs and angular (Frontend)
+    - nodejs and php (Backend)
+    - docker
+    - aws (Infrastructure and different services like s3, cloudfront, lambdas, rekognition and more)
+    - relational databases (mysql, sql server, oracle database)
+    - non-relational databases (dynamodb, elasticsearch, mongodb, redis)
+    `;
+    return stdout + "\n" + description;
   } else {
     return stdout + "\n" + "sh: command not found: " + command;
   }
@@ -35,7 +69,8 @@ const App = () => (
     <div class="bg-manjaro bg-center pt-5 px-5 pb-16 sm:p-20 h-full">
       <Toolbar title="Terminal" />
       <Console 
-        descriptionToWrite={"Hello my name is Fernando Velcic\nWelcome to my page\n~ >>>\t"}
+        descriptionToWrite={description}
+        autotyperSpeed={50}
         commandHandler={commandHandler}
       />
     </div>
--- src/Console.js
+++ src/Console.js
@@ -1,8 +1,9 @@
 import { useEffect, useState, useRef } from "react";
 
-const Console = ({prompt, descriptionToWrite, commandHandler}) => {
+const Console = ({prompt, descriptionToWrite, autotyperSpeed, commandHandler}) => {
   prompt = prompt || '\n~ >>>\t'
   descriptionToWrite = descriptionToWrite || prompt;
+  autotyperSpeed = autotyperSpeed || 100;
   commandHandler = commandHandler || (() => {});
 
   const [stdin, setStdin] = useState("");
@@ -64,7 +65,7 @@ const Console = ({prompt, descriptionToWrite, commandHandler}) => {
 
         increaseOffset();
         setStdout(stdout + descriptionToWrite[offset]);
-      }, 100);
+      }, autotyperSpeed);
 
     return () => clearInterval(intervalId); 
   }, [stdout]);
@@ -82,7 +83,7 @@ const Console = ({prompt, descriptionToWrite, commandHandler}) => {
   return (
     <div class="flex justify-end flex-col text-center h-full bg-black opacity-90 cursor-text" onClick={focusStdin}>
       <div class="p-1 font-mono text-left text-white whitespace-pre-line overflow-hidden overflow-y-auto scrollbar-thin scrollbar-thumb-rounded scrollbar-thumb-gray-100 scrollbar-track-gray-900">
-        {stdout}
+        <div class="inline" dangerouslySetInnerHTML={{ __html: stdout }}></div>
         <input
           type="text" name="input" autoComplete="off" autoFocus
           class="inline text-white bg-black focus:outline-none caret-black" style={{ width: stdin.length + 'ch' }}

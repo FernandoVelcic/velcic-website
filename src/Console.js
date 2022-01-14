054b738e87d1e67134762ceebc761ea78945aa8a
FernandoVelcic <fvelcic@gmail.com>
2022-01-14 16:29:50 -0300

    Improve autotyper

--- src/Console.js
+++ src/Console.js
@@ -1,10 +1,10 @@
-import { useEffect, useState, useRef } from "react";
+import { useEffect, useCallback, useState, useRef } from "react";
 
-const Console = ({prompt, descriptionToWrite, autotyperSpeed, commandHandler}) => {
+const Console = ({ prompt, descriptionToWrite, autotyperSpeed, commandHandler }) => {
   prompt = prompt || '\n~ >>>\t'
   descriptionToWrite = descriptionToWrite || prompt;
   autotyperSpeed = autotyperSpeed || 100;
-  commandHandler = commandHandler || (() => {});
+  commandHandler = commandHandler || (() => { });
 
   const [stdin, setStdin] = useState("");
   const stdinRef = useRef();
@@ -15,14 +15,9 @@ const Console = ({prompt, descriptionToWrite, autotyperSpeed, commandHandler}) =
 
   const [offset, setOffset] = useState(0);
 
-
-  const increaseOffset = () => {
-    setOffset(offset + 1);
-  };
-
-  const isTyping = () => {
+  const isTyping = useCallback(() => {
     return offset < descriptionToWrite.length;
-  };
+  }, [offset, descriptionToWrite]);
 
   const focusStdin = () => {
     stdinRef.current.focus();
@@ -35,7 +30,8 @@ const Console = ({prompt, descriptionToWrite, autotyperSpeed, commandHandler}) =
   }
 
   const onKeyDownStdin = (e) => {
-    if (isTyping()) return e.preventDefault();
+    const ignoreKeys = ['ArrowLeft', 'ArrowRight', 'ArrowUp', 'ArrowDown'];
+    if (isTyping() || ignoreKeys.includes(e.key)) return e.preventDefault();
 
     if (e.key === 'Enter') {
       const command = e.target.value;
@@ -51,24 +47,23 @@ const Console = ({prompt, descriptionToWrite, autotyperSpeed, commandHandler}) =
 
   //Scroll to bottom
   useEffect(() => {
-    if (isTyping()) return;
-      stdinRef.current.scrollIntoView();
+    stdinRef.current.scrollIntoView();
   }, [stdout]);
 
   //Autotyping
   useEffect(() => {
     const intervalId = setInterval(() => {
-        if (!isTyping()) {
-          clearInterval(intervalId);
-          return;
-        }
+      if (!isTyping()) {
+        clearInterval(intervalId);
+        return;
+      }
 
-        increaseOffset();
-        setStdout(stdout + descriptionToWrite[offset]);
-      }, autotyperSpeed);
+      setOffset(offset + 1);
+      setStdout(stdout + descriptionToWrite[offset]);
+    }, autotyperSpeed);
 
-    return () => clearInterval(intervalId); 
-  }, [stdout]);
+    return () => clearInterval(intervalId);
+  }, [stdout, offset, isTyping, autotyperSpeed, descriptionToWrite]);
 
   //Caret blinking
   useEffect(() => {
@@ -76,8 +71,8 @@ const Console = ({prompt, descriptionToWrite, autotyperSpeed, commandHandler}) =
       caretRef.current.style.display = (caretRef.current.style.display === "none" || isTyping()) ? "inline" : "none";
     }, 800);
 
-    return () => clearInterval(intervalId); 
-  }, [stdout]);
+    return () => clearInterval(intervalId);
+  }, [stdout, isTyping]);
 
 
   return (
@@ -85,7 +80,7 @@ const Console = ({prompt, descriptionToWrite, autotyperSpeed, commandHandler}) =
       <div class="p-1 font-mono text-left text-white whitespace-pre-line overflow-hidden overflow-y-auto scrollbar-thin scrollbar-thumb-rounded scrollbar-thumb-gray-100 scrollbar-track-gray-900">
         <div class="inline" dangerouslySetInnerHTML={{ __html: stdout }}></div>
         <input
-          type="text" name="input" autoComplete="off" autoFocus
+          type="text" name="input" autoComplete="off" maxlength="30" autoFocus
           class="inline text-white bg-black focus:outline-none caret-black" style={{ width: stdin.length + 'ch' }}
           value={stdin}
           ref={stdinRef}
